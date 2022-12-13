8c40014f7d65de969e2fbb95d2ea921bec03c3a3
Brandon Jacobs <brandon.jacobs345@gmail.com>
2022-12-12 18:00:53 -0700

    big commit

--- recipeBook/urls.py
+++ recipeBook/urls.py
@@ -3,24 +3,40 @@ from .views import indexPageView
 from .views import createPageView
 from .views import showSingleRecipePageView
 from .views import readPageView
-from .views import updatePageView
-from .views import addRecipePageView
+from .views import updateRecipePageView
+from .views import saveNewRecipePageView
 from .views import showSingleIngPageView
 from .views import updateIngPageView
-from .views import addIngPageView
-from .views import updateRecipeIngredient
-
+from .views import saveNewIngredientPageView
+from .views import addIngredientPageView
+from .views import viewRecipesPageView
+from .views import viewIngredientsPageView
+from .views import addRecipePageView
+from .views import deleteIngredientPageView
+from .views import deleteRecipePageView
+from .views import chooseIngredients
+from .views import addRecipeIngredientsPageView
+from .views import saveNewRecipeIngredientPageView
 urlpatterns = [
     path("", indexPageView, name="index"),
     path("create", createPageView, name="create"),
     path("read", readPageView, name="read"),
-    path('showRecipe/<int:rec_id>/', showSingleRecipePageView, name='update'),
-    path('updateRecipe/', updatePageView, name='updateRec'),
-    path('addRecipe/', addRecipePageView, name='addRecipe'),
-    path("showIng/", showSingleIngPageView, name="showIng"),
+    path('showRecipe/<int:recID>/', showSingleRecipePageView, name='showRecipe'),
+    path('updateRecipe/', updateRecipePageView, name='updateRec'),
+    path('saveNewRecipe/', saveNewRecipePageView, name='saveNewRecipe'),
+    path("showIngredient/<int:ingID>", showSingleIngPageView, name="showIngredient"),
     path("updateIng/", updateIngPageView, name="updateIng"),
-    path("addIng/", addIngPageView, name="addIng"), 
-    path('updateRecIng/<int:rec_id>/', updateRecipeIngredient, name = 'updateRecIng'),
+    path("saveNewIngredient/", saveNewIngredientPageView, name="saveNewIngredient"), 
+    path('recipes/', viewRecipesPageView, name='viewRecipes'),
+    path('ingredients/', viewIngredientsPageView, name='viewIngredients'),
+    path('addIngredient/', addIngredientPageView, name='addIngredient'),
+    path('addRecipe/', addRecipePageView, name = 'addRecipe'),
+    path('deleteIngredient/<int:ingID>', deleteIngredientPageView,name='deleteIngredient'),
+    path('deleteRecipe/<int:recID>', deleteRecipePageView, name='deleteRecipe'),
+    path('chooseIngredients/', chooseIngredients, name='chooseIngredients'),
+    path('addRecipeIngredient/', addRecipeIngredientsPageView, name='addRecipeIngredient'),
+    path('saveRecipeIngredients/', saveNewRecipeIngredientPageView, name = 'saveRecipeIngredient')
+
 
 ]
 
--- recipeBook/views.py
+++ recipeBook/views.py
@@ -12,78 +12,158 @@ def createPageView(request) :
 def readPageView(request) :
     return render(request, 'recipeBook/read.html') 
 
-def showSingleRecipePageView(request, rec_id) :
-    recipe = Recipe.objects.get(id=rec_id)
-
-    context = {
-        'rcp': recipe
-    }
-    return render(request, 'recipeBook/updateRecipe.html', context)
-
-def updatePageView(request):
+def showSingleRecipePageView(request, recID) :
     if request.method == 'POST':
-        rec_id = request.POST['rec_id']
+        recipeIngredients = Recipe_Ingredient.objects.filter(RecipeID=recID)
+        recipe = Recipe.objects.get(id=recID)
+        
+        # ingredientsAll = Ingredient.objects.all()
+        # ingredientsUnused = Ingredient.objects.exclude(RecipeID = recID)
+        # ingredientUsed = Ingredient.objects.exclude(RecipeID = recID)
 
+        context = {
+            'rI': recipeIngredients,
+            'rcp': recipe
+        }
+    return render(request, 'recipeBook/showSingleRecipe.html', context)
+
+def updateRecipePageView(request):
+    if request.method == 'POST':
+        rec_id = request.POST['rcpID']
         recipe = Recipe.objects.get(id = rec_id)
-
         recipe.RecipeTitle = request.POST['title']
         recipe.Preparation = request.POST['preparation']
         recipe.Notes = request.POST['notes']
-
         recipe.save()
-    return showSingleRecipePageView(request)
 
-def addRecipePageView(request):
+        ingredientIDList = request.POST.getlist('ingID')
+        amountList = request.POST.getlist('recIng')
+        for i in range(0, len(ingredientIDList)):
+            recipeIngredient = Recipe_Ingredient.objects.filter(RecipeID_id = rec_id)
+            recipeIngredient.get(IngredientID = ingredientIDList[i])
+            recipeIngredient.Amount = amountList[i]
+            recipeIngredient.save()
+
+    return showSingleRecipePageView(request, rec_id)
+
+def saveNewRecipePageView(request):
     if request.method == 'POST':
         recipe = Recipe()
         recipe.RecipeTitle = request.POST['title']
         recipe.Preparation = request.POST['preparation']
         recipe.Notes = request.POST['notes']
-
+        
         recipe.save()
-    return showSingleRecipePageView(request)
+        recID = recipe.id
+    return chooseIngredients(request, recID)
 
-def showSingleIngPageView(request):
-    ingredient = Ingredient.objects.all()
+def showSingleIngPageView(request, ingID):
+    if request.method == 'POST':
+        ingredient = Ingredient.objects.get(id=ingID)
+    else:
+        ingredient = Ingredient.objects.get(id=ingID)
     context = {
-        'ing' : ingredient
+    'ing' : ingredient
     }
-    return render(request, 'recipeBook/updateIngredients.html', context)
+    return render(request, 'recipeBook/showSingleIngredient.html', context)
 
 def updateIngPageView(request) :
     if request.method == 'POST':
-        ing_id = request.POST['ing_id']
+        ing_id = request.POST['ingID']
         ingredient = Ingredient.objects.get(id = ing_id)
-        ingredient.IngredientName= request.POST['name']
+        ingredient.IngredientName = request.POST['name']
         ingredient.save()
-    return showSingleIngPageView(request) 
+    return viewIngredientsPageView(request)
 
-def addIngPageView(request) :
+def saveNewIngredientPageView(request) :
     if request.method == 'POST':
         ingredient = Ingredient()
         ingredient.IngredientName= request.POST['name']
         ingredient.save()
-    return showSingleIngPageView(request)
+        
+    return viewIngredientsPageView(request)
+
+def addIngredientPageView(request):
+    return render(request, 'recipeBook/addIngredient.html')
+
+def addRecipePageView(request):
+    return render(request, 'recipeBook/addRecipe.html')
 
 def updateRecipeIngredient(request, rec_id):
     recipe = Recipe.objects.get(id=rec_id)
     recIngredients = Recipe_Ingredient.objects.filter(RecipeID_id = rec_id)
     ingredients = Ingredient.objects.all()
+    
     context = {
         'rec': recipe,
         'recIng': recIngredients,
-        'ing': ingredients
+        'ing': ingredients,
+        
     }
 
     return render(request, 'recipeBook/updateRecipeIngredients.html', context)
-def addRecipeIngredient(request):
-    if request.method == 'POST':
-        rec_id = request.POST['rec_id']
-        recipeIngredient = Recipe_Ingredient()
-        recipeIngredient. = request.POST['name']
-        ingredient.save()
 
-    return updateRecipeIngredient(request)
+def viewRecipesPageView(request):
+    recipes = Recipe.objects.all()
+    context = {
+        'rcp' : recipes
+    }
+    return render(request, 'recipeBook/showRecipes.html', context)
+
+def viewIngredientsPageView(request):
+    ingredients = Ingredient.objects.all()
+    
+    context = {
+        'ing' : ingredients
+    }
+    return render(request, 'recipeBook/showIngredients.html', context)
+
+def deleteIngredientPageView(request, ingID):
+    ing = Ingredient.objects.get(id=ingID)
+    ing.delete()
+    
+    return viewIngredientsPageView(request)
+
+def deleteRecipePageView(request, recID):
+    rec = Recipe.objects.get(id=recID)
+    rec.delete()
+
+    return viewRecipesPageView(request)
+
+def chooseIngredients(request, recID):
+    ingredients = Ingredient.objects.all()
+    context = {
+        'ingredients' : ingredients,
+        'recID' : recID
+    }
+    return render(request, 'recipeBook/chooseIngredients.html', context)
+
+def addRecipeIngredientsPageView(request):
+    if request.method == 'POST':
+        recID = request.POST['recID']
+        ingredientList = request.POST.getlist('array')
+        ingredients = Ingredient.objects.filter(id__in = ingredientList)
+
+        context =  {
+            'recID' : recID,
+            'ingredients': ingredients
+        }
+
+    return render(request, 'recipeBook/addRecipeIngredient.html', context)
+
+def saveNewRecipeIngredientPageView(request):
+    if request.method == 'POST':
+        recID = request.POST['recID']
+        ingredientList = request.POST.getlist('ingID')
+        amountList = request.POST.getlist('recIng')
+        for i in range(0, len(ingredientList)):
+            recipeIngredient = Recipe_Ingredient()
+            recipeIngredient.RecipeID_id = recID
+            recipeIngredient.IngredientID_id = ingredientList[i]
+            recipeIngredient.Amount = amountList[i]
+            recipeIngredient.save()
+    return showSingleRecipePageView(request, recID)
+
 
 
 
